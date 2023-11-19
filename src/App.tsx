6f58aa42f18e226a8a1642ff2730e3032e707a1e
Faizan Raza <faizanms025347@gmail.com>
2023-11-19 21:19:43 +0530

    Added all the files

---
+++ src/App.tsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react"
+import "./App.css"
+// import { type } from "os";
+
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<
+  Note[]>
+  ([ ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [selectedNote, setSelectedNote] =
+     useState<Note | null>(null);
+
+  const handleNoteClick = (note:Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  }
+
+  const handleAddNote = (
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content
+
+    }
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
+
+  const handleUpdateNote = (
+    event : React.FormEvent
+  ) => {
+    event.preventDefault();
+
+    if(!selectedNote){
+      return;
+    }
+
+    const updatedNote: Note = {
+      id: selectedNote.id,
+      title: title,
+      content: content,
+    }
+
+    const updatedNotesList = notes.map((note) =>
+    note.id === selectedNote.id
+       ? updatedNote
+       : note
+    )
+    setNotes(updatedNotesList)
+    setTitle("")
+    setContent("")
+    setSelectedNote(null);
+  };
+
+  const handleCancle = () => {
+    setTitle("")
+    setContent("")
+    setSelectedNote(null)
+  }
+
+  const deleteNote = (
+    event: React.MouseEvent, 
+    noteId: number
+    ) => {
+      event.stopPropagation();
+
+      const updateNotes = notes.filter(
+        (note)=> note.id !== noteId
+      )
+
+      setNotes(updateNotes);
+  }
+
+  return( 
+    <div className="app-container">
+      <form className="note-form" 
+      onSubmit={(event) => 
+        selectedNote
+        ? handleUpdateNote(event)
+        : handleAddNote(event)}>
+
+        <input 
+        value={title} 
+        onChange={(event)=>
+          setTitle(event.target.value)
+        }
+        placeholder="Title" required>
+        </input>
+        <textarea className="txt" 
+        value={content} 
+        onChange={(event)=>
+          setContent(event.target.value)
+        } 
+        placeholder="content" rows={10} required>
+        </textarea>
+        
+        {selectedNote ? (
+          <div className="edit-buttons">
+          <button type="submit">Save</button>
+          <button type="submit" onClick={handleCancle}>
+            Cancle
+            </button>
+          </div>
+        ) : (
+        <button type="submit" >Add Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) =>(
+          <div className="note-item"
+          onClick={() => handleNoteClick(note)}
+          >
+          <div className="notes-header">
+            <button 
+            onClick={(event) =>
+            deleteNote(event, note.id)
+            }
+            >X</button>
+          </div>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+        ))}
+        
+      </div>
+    </div>
+   )
+}
+
+export default App
